2586170eaa0accc7a567e7241aac4ce3a593b6a7
Divy Jani <divyjani06@gmail.com>
2026-02-18 23:59:08 +0530

    error resolved

--- services/User_Services.py
+++ services/User_Services.py
@@ -1,7 +1,12 @@
+from core.config import db
+
 class User_Service:
+    def __init__(self):
+            pass
 
     def Find_Users():
+        
         print("User found successfully")
         
     def Find_All_Users():
-        print("Users Found Successfully")
+        print("Users Found Successfully and It has to be")
