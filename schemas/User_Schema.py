aa4a7d478c7ab9413459b196294646ccddc718c9
Divy Jani <divyjani06@gmail.com>
2026-03-11 23:55:35 +0530

    Updation and schema design based on req

--- schemas/User_Schema.py
+++ schemas/User_Schema.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
 class UserSchema:
     name: str
     email: str
+    phone: Optional[str] = None
+    company: Optional[str] = None
+    referral_code: Optional[str] = None
