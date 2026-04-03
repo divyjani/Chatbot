a5a62f2749cd28a5cb0af25af932ceb8d0aa1f15
divyjani <divyjani06@gmail.com>
2026-04-03 23:30:31 +0530

    little changes in api

--- schemas/Auth_Schema.py
+++ schemas/Auth_Schema.py
@@ -3,9 +3,12 @@ from pydantic import BaseModel, EmailStr
 class LoginSchema(BaseModel):
     email: EmailStr
     password: str
+    # role:str
     
 class RegisterSchema(LoginSchema):
     Fullname:str 
     
 class TokenSchema(BaseModel):
     token:str
+
+class AdminSchema(BaseModel):
